d38c0df51fbbc98dc712bdabdad2e4a197a853df
Natalia Zolotukhina <nzolotukhina@nzolotukhina-macbookair.roam.corp.google.com>
2015-12-07 10:05:17 -0800

    Hack FT runner to be able to test staging

--- lists/migrations/0003_list.py
+++ lists/migrations/0003_list.py
@@ -16,8 +16,5 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
             ],
-            options={
-            },
-            bases=(models.Model,),
         ),
     ]
